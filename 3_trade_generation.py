275d9e7663bf583bcd1f28ecd33f39868675b30a
jmbrittain25 <jmbrittain25@comcast.net>
2025-09-01 17:05:23 -0400

    generating big boi model trade

--- 3_trade_generation.py
+++ 3_trade_generation.py
@@ -4,14 +4,14 @@ from random import shuffle
 
 def generate_combinations():
     model_sizes = [
-        {'n_layers': 2, 'n_heads': 2, 'd_model': 128, 'd_ff': 512},
+        # {'n_layers': 2, 'n_heads': 2, 'd_model': 128, 'd_ff': 512},
         {'n_layers': 4, 'n_heads': 4, 'd_model': 256, 'd_ff': 1024},
         {'n_layers': 6, 'n_heads': 8, 'd_model': 512, 'd_ff': 2048},
-        # {'n_layers': 8, 'n_heads': 16, 'd_model': 1024, 'd_ff': 4096}  # too big for m2 mac mini to run reasonably fast
+        {'n_layers': 8, 'n_heads': 16, 'd_model': 1024, 'd_ff': 4096}  # too big for m2 mac mini to run reasonably fast
     ]
     # dataset_sizes = [100, 500, 1_000, 5_000]
-    dataset_sizes = [10_000] # , 50_000, 100_000]
-    n_bins_list = [128, 512, 2_048, 8_192]  # [128, 256, 512, 1_024, 2_048, 4_096, 8_192, 16_384]
+    dataset_sizes = [10_000, 50_000, 100_000]
+    n_bins_list = [2_048, 8_192, 16_384]  # [128, 256, 512, 1_024, 2_048, 4_096, 8_192, 16_384]
     coordinate_systems = ['spherical', 'cartesian']
     lr_list = [1e-3, 1e-4, 1e-5]
     batch_sizes = [16, 32, 64]
@@ -47,7 +47,7 @@ def generate_bash_files(combinations):
     combs_per_machine = len(combinations) // N_MACHINES
     machine_combs = [combinations[i * combs_per_machine:(i + 1) * combs_per_machine] for i in range(N_MACHINES)]
     for machine_id, combs in enumerate(machine_combs):
-        script_path = os.path.join(TARGET_DIR, f"run_machine_{machine_id}.sh")
+        script_path = os.path.join(TARGET_DIR, f"run_big_model_machine_{machine_id}.sh")
         with open(script_path, 'w') as f:
             f.write("#!/bin/bash\n")
             for comb in combs:
@@ -61,7 +61,7 @@ def generate_bash_files(combinations):
 
 if __name__ == "__main__":
 
-    N_MACHINES = 4
+    N_MACHINES = 1
     TARGET_DIR = "."
 
     combinations = generate_combinations()
